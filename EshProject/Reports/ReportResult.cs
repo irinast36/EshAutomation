4e523218a200cfcb8b9b45ea36e39505bb8f3987
irinast36 <irinastruzberg@gmail.com>
2024-08-14 14:36:29 -0700

    Add files via upload

---
+++ EshProject/Reports/ReportResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EshProject
+{
+    public class ReportResult
+    {
+        public void LogResultToFile(OperationResult result)
+        {
+            string filePath = "testResults.log";
+            string logMessage = $"Test executed at {result.RunTime}: Test Result = {(result.Message)}";
+
+            try
+            {
+                File.AppendAllText(filePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error logging to file: {ex.Message}");
+            }
+        }
+    }
+}
